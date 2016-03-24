f8fa4139d2aa39362f4a712356effce1819c22b9
Tony Spencer <mobiletony@msn.com>
2016-03-23 22:13:41 -0600

    adding the app.core back in.

---
+++ DotNetForum/App.Core/Security/IPrincipalExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core.Security
+{
+    public static class IPrincipalExtension
+    {
+        public static bool HasAnyRole(this IPrincipal user, params AppRole[] roles)
+        {
+            var roleArray = roles.ToStringArray();
+            var result = roleArray.Any(user.IsInRole);
+            return result;
+        }
+
+        public static string GetEmail(this IPrincipal user)
+        {
+            var claimsUser = user as ClaimsPrincipal;
+            var email = claimsUser.FindFirst(ClaimTypes.Email).Value;
+            return email;
+        }
+    }
+}
